68a6d7d81422c76e294b98d15fac9b65070afebf
YimoEx <205423185+chunkburst@users.noreply.github.com>
2025-05-13 17:14:38 +0800

    Fix: "[]" error

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-XRAYR_DIR="/root/Xray"
+XRAYR_DIR="/root/Xray/src"
 XRAYR_EXEC="${XRAYR_DIR}/XrayR"
 XRAYR_CONFIG="${XRAYR_DIR}/config.yml"
 SERVICE_NAME="xrayr.service"
@@ -9,7 +9,7 @@ SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}"
 echo "开始设置 XrayR 守护进程..."
 
 # 检查程序文件是否存在
-if [ ! -f "$XRAYR_EXEC" || ! -f "$XRAYR_CONFIG" ]; then
+if [[ ! -f "$XRAYR_EXEC" || ! -f "$XRAYR_CONFIG" ]]; then
     echo "错误：找不到 XrayR 可执行文件在 ${XRAYR_EXEC} or 无Config (${$XRAYR_CONFIG})"
     echo "请确保文件存在 && 有执行权限"
     exit 1
